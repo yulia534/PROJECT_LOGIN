477200224a5d2b353fc8b9801be479139c4dae9a
yulia534 <yulia100506@gmail.com>
2025-10-02 11:28:14 +0700

    proses login

--- login.php
+++ login.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+
+// Cek apakah user sudah login
+if (isset($_SESSION['username'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+
+// Proses login saat form dikirim
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // Login sederhana (username: admin, password: 123)
+    if ($username == 'yulia' && $password === '12345') {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'Pegawai';
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
